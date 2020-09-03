ff67837b953457c6cbe5592a3d49dcf41d6cbc82
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-09-03 00:01:58 -0500

    Early commit with just file heading comments and ifndef for files.

---
+++ functions.h
@@ -0,0 +1,12 @@
+/*********************
+Name: Allison Crenshaw
+Coding 02
+Purpose: Classes and objects refresher assignment.
+**********************/
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+
+#endif
