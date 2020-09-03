491d70af195b65c282d175aa9496ddc2d26a89ef
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-09-03 00:02:41 -0500

    Early commit with just file heading comments and ifndef for files.

---
+++ fighterShip.h
@@ -0,0 +1,11 @@
+/*********************
+Name: Allison Crenshaw
+Coding 02
+Purpose: Classes and objects refresher assignment.
+**********************/
+
+#ifndef FIGHTER_SHIP_H
+#define FIGHTER_SHIP_H
+
+
+#endif
---
+++ spaceship.h
@@ -0,0 +1,12 @@
+/*********************
+Name: Allison Crenshaw
+Coding 02
+Purpose: Classes and objects refresher assignment.
+**********************/
+
+#ifndef SPACESHIP_H
+#define SPACESHIP_H
+
+
+
+#endif
