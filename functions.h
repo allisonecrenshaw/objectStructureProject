aed88bf3a3ca88031d2a76510e426a3cd7f7b6f9
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 10:32:04 -0600

    Includes added and prototype for displaySpaceship added.

--- functions.h
+++ functions.h
@@ -7,6 +7,11 @@ Purpose: Classes and objects refresher assignment.
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <iostream>
+#include "fighterShip.h"
+using std::cout;
+using std::endl;
 
+void displaySpaceship(Spaceship*);
 
 #endif
