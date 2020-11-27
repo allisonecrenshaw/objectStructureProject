fafbb3d48b214e786fdc1cd0c3d2a7b5d80f4070
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 10:39:07 -0600

    All attributes present and prototypes written.

--- fighterShip.h
+++ fighterShip.h
@@ -7,5 +7,27 @@ Purpose: Classes and objects refresher assignment.
 #ifndef FIGHTER_SHIP_H
 #define FIGHTER_SHIP_H
 
+#include "spaceship.h"
+
+class Fightership : public Spaceship {
+public:
+    Fightership();
+    Fightership(int, int, int, int);
+    ~Fightership();
+
+    // Getters
+    int getFightPower;
+    int getExtraSpeed;
+    int getTurboSpeed;
+
+    // Setters
+    void setFightPower();
+    void setExtraSpeed();
+
+private:
+    int fightPower;
+    int extraSpeed;
+
+};
 
 #endif
