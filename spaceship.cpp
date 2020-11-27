749246e3121f23e9f5861d59b3a8f7a55c989346
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 09:33:10 -0600

    Constructors and destructor added, getters added.

--- spaceship.cpp
+++ spaceship.cpp
@@ -5,3 +5,29 @@ Purpose: Classes and objects refresher assignment.
 **********************/
 
 #include "spaceship.h"
+
+Spaceship::Spaceship() {
+    setWeight(0);
+    setEnginePower(0)
+}
+
+Spaceship::Spaceship(int w, int eP) {
+    setWeight(w);
+    setEnginePower(eP);
+}
+
+Spaceship::~Spaceship() {
+    // not used in this assignment
+}
+
+int Spaceship::getWeight() {
+    return weight;
+};
+
+int Spaceship::getEnginePower() {
+    return enginePower;
+}
+
+int Spaceship::getSpeed() {
+    return enginePower-weight;
+}
