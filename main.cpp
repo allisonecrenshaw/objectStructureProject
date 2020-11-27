f6e031e26377194cf17b2b4b7cd5f881475f5857
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 10:23:58 -0600

    Main function created and first couple calls made to create Spaceship objects.

--- main.cpp
+++ main.cpp
@@ -5,3 +5,20 @@ Purpose: Classes and objects refresher assignment.
 **********************/
 
 #include "main.h"
+
+int main() {
+    string line = "***********************************\n";
+
+    cout << line;
+    cout << "Start Spaceship Program." << endl;
+    cout << line;
+
+    cout << "Creating new Spaceship w/o attributes: ";
+    Spaceship ship1;
+
+    cout << "Creating new Spaceship with attributes: ";
+    Spaceship ship2(3, 10);
+
+
+    return 0;
+}
