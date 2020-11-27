c56af3760e4386c48929c27b5189466e6802293f
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 10:42:12 -0600

    Functions stubbed out.

--- fighterShip.cpp
+++ fighterShip.cpp
@@ -5,3 +5,35 @@ Purpose: Classes and objects refresher assignment.
 **********************/
 
 #include "fighterShip.h"
+
+Fightership::Fightership() {
+
+}
+
+Fightership::Fightership(int w, int eP, int fP, int eS) {
+
+}
+
+Fightership::~Fightership() {
+
+}
+
+int Fightership::getFightPower() {
+
+}
+
+int Fightership::getExtraSpeed() {
+
+}
+
+int Fightership::getTurboSpeed() {
+
+}
+
+void Fightership::setFightPower() {
+
+}
+
+void Fightership::setExtraSpeed() {
+    
+}
