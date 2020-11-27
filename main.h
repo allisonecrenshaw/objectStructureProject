d18d4b14c6d9b45dff963f6c6f3043e32cb52e51
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 10:23:04 -0600

    Main function created and first couple calls made to create Spaceship objects.

--- main.h
+++ main.h
@@ -7,6 +7,15 @@ Purpose: Classes and objects refresher assignment.
 #ifndef MAIN_CLASSES_H
 #define MAIN_CLASSES_H
 
+#include "spaceship.h"
+#include "fighterShip.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "string"
+using std::string;
 
 
 #endif
