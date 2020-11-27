a38c70af0021062f953ebe2b4085aefdb91ededd
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 11:16:23 -0600

    functions complete.

--- spaceship.cpp
+++ spaceship.cpp
@@ -8,7 +8,7 @@ Purpose: Classes and objects refresher assignment.
 
 Spaceship::Spaceship() {
     setWeight(0);
-    setEnginePower(0)
+    setEnginePower(0);
 }
 
 Spaceship::Spaceship(int w, int eP) {
@@ -31,3 +31,20 @@ int Spaceship::getEnginePower() {
 int Spaceship::getSpeed() {
     return enginePower-weight;
 }
+
+void Spaceship::setWeight(int w) {
+    if (w > 0) {
+        weight = w;
+    } else {
+        weight = 0;
+    }
+}
+
+void Spaceship::setEnginePower(int eP) {
+    if (eP > 0) {
+        enginePower = eP;
+    } else {
+        enginePower = 0;
+    }
+}
+
