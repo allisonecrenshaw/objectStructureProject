79436392392c605dba906814788885e6b454b42b
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 10:30:55 -0600

    Include for spaceships removed, include for functions added.

--- main.h
+++ main.h
@@ -7,8 +7,8 @@ Purpose: Classes and objects refresher assignment.
 #ifndef MAIN_CLASSES_H
 #define MAIN_CLASSES_H
 
-#include "spaceship.h"
 #include "fighterShip.h"
+#include "functions.h"
 
 #include <iostream>
 using std::cout;
