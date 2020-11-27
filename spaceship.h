91e598fe8580f4ce7d7877ff79bbf58800096047
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 09:32:36 -0600

    All prototypes and data type attributes written out.

--- spaceship.h
+++ spaceship.h
@@ -7,6 +7,27 @@ Purpose: Classes and objects refresher assignment.
 #ifndef SPACESHIP_H
 #define SPACESHIP_H
 
+class Spaceship {
 
+public:
+    // constructors & destructor
+    Spaceship();
+    Spaceship (int, int);
+    ~Spaceship();
+
+    // Getters
+    int getWeight();
+    int getEnginePower();
+    int getSpeed();
+
+    // Setters
+    void setWeight(int);
+    void setEnginePower(int);
+
+protected:
+    // private variables that define spaceship
+    int weight, enginePower;
+
+}; // end class Spaceship
 
 #endif
