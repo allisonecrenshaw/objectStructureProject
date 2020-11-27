e8fa838e287c429a967bfa3a985b64bfe44e8766
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 10:31:15 -0600

    Call to display spaceships added.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,29 @@ Purpose: Classes and objects refresher assignment.
 int main() {
     string line = "***********************************\n";
 
+    cout << endl;
     cout << line;
     cout << "Start Spaceship Program." << endl;
     cout << line;
 
-    cout << "Creating new Spaceship w/o attributes: ";
+    cout << "Creating new Spaceship w/o attributes: " << endl;
     Spaceship ship1;
 
-    cout << "Creating new Spaceship with attributes: ";
+    cout << "Creating new Spaceship with attributes: " << endl;
     Spaceship ship2(3, 10);
 
+    cout << endl;
+    cout << "Displaying characteristics of Ship 1: " << endl;
+    displaySpaceship(&ship1);
+    cout << endl;
+    cout << "Displaying characteristics of Ship 2: " << endl;
+    displaySpaceship(&ship1);
+    cout << endl;
+
+    cout << "Code for fighter ships display will go here." << endl;
+    cout << endl;
+
+    cout << "fin." << endl;
 
     return 0;
 }
